54df4fde25e9ba9c9956f25e5a8828ec1628c088
William Stockhausen <williamstockhausen@MacBook-Pro.local>
2020-10-28 08:23:30 -0700

    Corrected error in getConnectivityZones. Incremented version to 2020.10.28.

--- R/getConnectivityZones.R
+++ R/getConnectivityZones.R
@@ -1,12 +1,12 @@
 #'
 #' @title Get a set of connectivity zones
 #' 
-#' @description Function to get a set of connectivity zones as a \code{sf} dataframe.
+#' @description Function to get a set of connectivity zones as a \pkg{sf} dataframe.
 #' 
 #' @param type - type of connectivity zone to get ("2020","30-layer","2019", or "10-layer")
 #' @param crs_type - type of crs ("LL" for lat/lon, "AA" for Alaska Albers projection)
 #' 
-#' @return \code{sf} dataframe with connectivity zones in given crs.
+#' @return \pkg{sf} dataframe with connectivity zones in given crs.
 #' 
 #' @details None.
 #' 
@@ -16,7 +16,7 @@ getConnectivityZones<-function(type=c("2020","30-layer","2019","10-layer"),
                                crs_type=c("LL","AA")){
   if (tolower(type[1]) %in% c("2020","30-layer")){
     message("Retrieving 2020/30-layer connectivity zones");
-    if (tolower(crs[1])=="ll"){
+    if (tolower(crs_type[1])=="ll"){
       message("\tin lat/lon coordinates");
       fn<-system.file(file.path("extdata","ConnectivityZones2020.LL.RData"),package="rIBMsSnowCrab");
     } else {
@@ -25,7 +25,7 @@ getConnectivityZones<-function(type=c("2020","30-layer","2019","10-layer"),
     }
   } else if (tolower(type[1]) %in% c("2019","10-layer")){
     message("Retrieving 2019/10-layer connectivity zones");
-    if (tolower(crs[1])=="ll"){
+    if (tolower(crs_type[1])=="ll"){
       message("\tin lat/lon coordinates");
       fn<-system.file(file.path("extdata","ConnectivityZones2019.LL.RData"),package="rIBMsSnowCrab");
     } else {
